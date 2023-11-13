ffbb1b9f846e3e6678e840d12b3c1d28508536b4
Joris Baaij <jorisbaaij@Joriss-MacBook-Pro.local>
2023-11-13 19:34:08 +0100

    first commit Kanban

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+// import logo from "./logo.svg";
+import { useState } from "react";
+import "./App.css";
+import React from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  // Function to increment the count
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  // Function to decrement the count
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={incrementCount}>Increment</button>
+      <button onClick={decrementCount}>Decrement</button>
     </div>
   );
 }
 
 export default App;
+
+/* <header className="App-header">
+<img src={logo} className="App-logo" alt="logo" />
+<p>
+  Edit <code>src/App.tsx</code> and save to reload.
+</p>
+<a
+  className="App-link"
+  href="https://reactjs.org"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Learn React
+</a>
+</header>
+*/
